b150843bfb5547ff89a22840b4285aa752153687
James Adcock <jamesadcock1980@gmail.com>
2024-05-16 23:30:12 +0100

    new controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,16 @@ public class PlayerController : MonoBehaviour
   
     private Rigidbody2D _rb2d;
     private bool _canMove = true;
+    private Animator _anim;
+    private Direction _direction;
+    private bool _moveLeft;
+    private bool _moveRight;
+
+    enum Direction
+    {
+        Left,
+        Right
+    }
 
     public void CanMove(bool canMove)
     {
@@ -20,15 +30,68 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (_canMove)
         {
-            Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            _rb2d.MovePosition(_rb2d.position + movementVector * Time.deltaTime * MovementSpeed);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _anim.SetBool("IsRowing", true);
+
+                if(_direction == Direction.Left)
+                {
+                    _moveLeft = true;
+                    _moveRight = false;
+                }
+
+                if(_direction == Direction.Right)
+                {
+                    _moveLeft = false;
+                    _moveRight = true;
+                }
+                
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                _anim.SetBool("IsRowing", false);
+                _moveRight = false;
+                _moveLeft = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                _anim.SetBool("IsRowing", false);
+                _anim.SetBool("IsLeft", true);
+                _anim.SetBool("IsRight", false);
+                _direction = Direction.Right;
+                _moveRight = false;
+                _moveLeft = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                _anim.SetBool("IsRowing", false);
+                _anim.SetBool("IsRight", true);
+                _anim.SetBool("IsLeft", false);
+                _direction = Direction.Left;
+                _moveRight = false;
+                _moveLeft = false;
+            }
+
+            if (_moveRight)
+            {
+                _rb2d.MovePosition(_rb2d.position + new Vector2(1, 0) * Time.deltaTime * MovementSpeed);
+            }
+
+            if (_moveLeft)
+            {
+                _rb2d.MovePosition(_rb2d.position + new Vector2(-1, 0) * Time.deltaTime * MovementSpeed);
+            }
         }
     }
 
