3af5b4d4cdc972ce1e7e07be14e0391cef46269a
James Adcock <jamesadcock1980@gmail.com>
2024-05-30 21:11:41 +0100

    Add facebook like button

---
+++ Assets/Scripts/SocialMedia.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialMedia : MonoBehaviour
+{
+   public void OnClickFaceBookLike()
+    {
+        Application.OpenURL("https://www.facebook.com/people/AquaRush/61560079332175/");
+    }
+}
